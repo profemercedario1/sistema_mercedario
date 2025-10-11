4ee5768f2e856bad06143b3243842e691c5e2357
Roman <profemercedario1@gmail.com>
2025-10-11 03:56:20 -0500

    Configurar conexión a PostgreSQL en Render

--- sistema_web/settings.py
+++ sistema_web/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'sistema_web.wsgi.application'
 # ========================
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )

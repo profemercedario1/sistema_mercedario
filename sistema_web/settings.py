1b9e476354c24fb3b8c26da04b0eb00132c3cac0
Roman <profemercedario1@gmail.com>
2025-10-11 03:34:56 -0500

    Eliminar archivo create_admin.py y actualizar configuración

--- sistema_web/settings.py
+++ sistema_web/settings.py
@@ -5,6 +5,7 @@ Generado por Django 5.2.7
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # ========================
 # RUTAS BASE
@@ -86,10 +87,11 @@ WSGI_APPLICATION = 'sistema_web.wsgi.application'
 # BASE DE DATOS
 # ========================
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # ========================
